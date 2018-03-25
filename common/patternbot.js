36747af32904d252c9f62b9add70fc003f4355ff
jswoon12 <jswoon12@hotmail.com>
2018-03-25 13:29:42 -0400

    Add banner, header, and footer

--- common/patternbot.js
+++ common/patternbot.js
@@ -101,6 +101,7 @@ const patternBotIncludes = function (manifest) {
     for (i = 0; i < t; i++) {
       if (rootMatcher.test(allScripts[i].src)) {
         return allScripts[i].src.split(rootMatcher)[0];
+        break;
       }
     }
   };
@@ -142,7 +143,7 @@ const patternBotIncludes = function (manifest) {
     let patternInfoJson;
     const data = patternElem.innerText.trim();
 
-    if (!data) return {};
+    if (!data) return {}
 
     try {
       patternInfoJson = JSON.parse(data);
@@ -171,50 +172,9 @@ const patternBotIncludes = function (manifest) {
     };
   };
 
-  const correctHrefPaths = function (html) {
-    const hrefSearch = /href\s*=\s*"\.\.\/\.\.\//g;
-    const srcSearch = /src\s*=\s*"\.\.\/\.\.\//g;
-    const urlSearch = /url\((["']*)\.\.\/\.\.\//g;
-
-    return html
-      .replace(hrefSearch, 'href="../')
-      .replace(srcSearch, 'src="../')
-      .replace(urlSearch, 'url($1../')
-    ;
-  };
-
-  const buildAccurateSelectorFromElem = function (elem) {
-    let theSelector = elem.tagName.toLowerCase();
-
-    if (elem.id) theSelector += `#${elem.id}`;
-    if (elem.getAttribute('role')) theSelector += `[role="${elem.getAttribute('role')}"]`;
-    if (elem.classList.length > 0) theSelector += `.${[].join.call(elem.classList, '.')}`;
-
-    theSelector += ':first-of-type';
-
-    return theSelector;
-  };
-
-  /**
-   * This is an ugly mess: Blink does not properly render SVGs when using DOMParser alone.
-   * But, I need DOMParser to determine the correct element to extract.
-   *
-   * I only want to get the first element within the `<body>` tag of the loaded document.
-   * This dumps the whole, messy, HTML document into a temporary `<div>`,
-   * then uses the DOMParser version, of the same element, to create an accurate selector,
-   * then finds that single element in the temporary `<div>` using the selector and returns it.
-   */
   const htmlStringToElem = function (html) {
-    let theSelector = '';
-    const tmpDoc = document.createElement('div');
-    const finalTmpDoc = document.createElement('div');
     const doc = (new DOMParser()).parseFromString(html, 'text/html');
-
-    tmpDoc.innerHTML = html;
-    theSelector = buildAccurateSelectorFromElem(doc.body.firstElementChild);
-    finalTmpDoc.appendChild(tmpDoc.querySelector(theSelector));
-
-    return finalTmpDoc;
+    return doc.body;
   };
 
   const replaceElementValue = function (elem, sel, data) {
@@ -237,7 +197,7 @@ const patternBotIncludes = function (manifest) {
 
     if (!patternDetails.html) return;
 
-    patternOutElem = htmlStringToElem(correctHrefPaths(patternDetails.html));
+    patternOutElem = htmlStringToElem(patternDetails.html);
     patternData = getPatternInfo(patternElem);
 
     Object.keys(patternData).forEach((sel) => {
@@ -274,7 +234,7 @@ const patternBotIncludes = function (manifest) {
   };
 
   const hideLoadingScreen = function () {
-    let allDownloadedInterval = setInterval(() => {
+    const allDownloadedInterval = setInterval(() => {
       if (Object.values(downloadedAssets).includes(false)) return;
 
       clearInterval(allDownloadedInterval);
@@ -387,10 +347,10 @@ const patternBotIncludes = function (manifest) {
 
 /** 
  * Patternbot library manifest
- * /Users/thomasjbradley/Dropbox/learn-the-web/web-dev-4/geohub
- * @version 1521465881687
+ * /Users/harryjeong/Documents/Semester 04/Web Development 04/week 10/geohub
+ * @version 1521998665982
  */
-const patternManifest_1521465881686 = {
+const patternManifest_1521998665981 = {
   "commonInfo": {
     "modulifier": [
       "responsive",
@@ -577,9 +537,7 @@ const patternManifest_1521465881686 = {
           "primary": 0,
           "opposite": 255
         }
-      },
-      "bodyRaw": "\nGeoHub specializes in selling geological marvels: gems, crystals, diamonds, geodes and much more!\n",
-      "bodyBasic": "GeoHub specializes in selling geological marvels: gems, crystals, diamonds, geodes and much more!"
+      }
     },
     "icons": [
       "crystal",
@@ -594,45 +552,50 @@ const patternManifest_1521465881686 = {
   },
   "patternLibFiles": {
     "commonParsable": {
-      "gridifier": "/Users/thomasjbradley/Dropbox/learn-the-web/web-dev-4/geohub/common/grid.css",
-      "typografier": "/Users/thomasjbradley/Dropbox/learn-the-web/web-dev-4/geohub/common/type.css",
-      "modulifier": "/Users/thomasjbradley/Dropbox/learn-the-web/web-dev-4/geohub/common/modules.css",
-      "theme": "/Users/thomasjbradley/Dropbox/learn-the-web/web-dev-4/geohub/common/theme.css"
+      "gridifier": "/Users/harryjeong/Documents/Semester 04/Web Development 04/week 10/geohub/common/grid.css",
+      "typografier": "/Users/harryjeong/Documents/Semester 04/Web Development 04/week 10/geohub/common/type.css",
+      "modulifier": "/Users/harryjeong/Documents/Semester 04/Web Development 04/week 10/geohub/common/modules.css",
+      "theme": "/Users/harryjeong/Documents/Semester 04/Web Development 04/week 10/geohub/common/theme.css"
     },
     "imagesParsable": {
-      "icons": "/Users/thomasjbradley/Dropbox/learn-the-web/web-dev-4/geohub/images/icons.svg"
+      "icons": "/Users/harryjeong/Documents/Semester 04/Web Development 04/week 10/geohub/images/icons.svg"
     },
     "logos": {
-      "sizeLarge": "/Users/thomasjbradley/Dropbox/learn-the-web/web-dev-4/geohub/images/logo.svg",
-      "size64": "/Users/thomasjbradley/Dropbox/learn-the-web/web-dev-4/geohub/images/logo-64.svg",
-      "size32": "/Users/thomasjbradley/Dropbox/learn-the-web/web-dev-4/geohub/images/logo-32.svg",
-      "size16": "/Users/thomasjbradley/Dropbox/learn-the-web/web-dev-4/geohub/images/logo-16.svg",
+      "sizeLarge": "/Users/harryjeong/Documents/Semester 04/Web Development 04/week 10/geohub/images/logo.svg",
+      "size64": "/Users/harryjeong/Documents/Semester 04/Web Development 04/week 10/geohub/images/logo-64.svg",
+      "size32": "/Users/harryjeong/Documents/Semester 04/Web Development 04/week 10/geohub/images/logo-32.svg",
+      "size16": "/Users/harryjeong/Documents/Semester 04/Web Development 04/week 10/geohub/images/logo-16.svg",
       "size16Local": "logo-16.svg",
       "size32Local": "logo-32.svg",
       "size64Local": "logo-64.svg",
       "sizeLargeLocal": "logo.svg"
     },
     "patterns": [
-      "/Users/thomasjbradley/Dropbox/learn-the-web/web-dev-4/geohub/patterns/banner",
-      "/Users/thomasjbradley/Dropbox/learn-the-web/web-dev-4/geohub/patterns/buttons",
-      "/Users/thomasjbradley/Dropbox/learn-the-web/web-dev-4/geohub/patterns/cards",
-      "/Users/thomasjbradley/Dropbox/learn-the-web/web-dev-4/geohub/patterns/footer",
-      "/Users/thomasjbradley/Dropbox/learn-the-web/web-dev-4/geohub/patterns/header",
-      "/Users/thomasjbradley/Dropbox/learn-the-web/web-dev-4/geohub/patterns/links"
+      "/Users/harryjeong/Documents/Semester 04/Web Development 04/week 10/geohub/patterns/banner",
+      "/Users/harryjeong/Documents/Semester 04/Web Development 04/week 10/geohub/patterns/buttons",
+      "/Users/harryjeong/Documents/Semester 04/Web Development 04/week 10/geohub/patterns/cards",
+      "/Users/harryjeong/Documents/Semester 04/Web Development 04/week 10/geohub/patterns/footer",
+      "/Users/harryjeong/Documents/Semester 04/Web Development 04/week 10/geohub/patterns/header",
+      "/Users/harryjeong/Documents/Semester 04/Web Development 04/week 10/geohub/patterns/links"
     ],
-    "pages": []
+    "pages": [
+      {
+        "name": "home.html",
+        "namePretty": "Home",
+        "path": "/Users/harryjeong/Documents/Semester 04/Web Development 04/week 10/geohub/pages/home.html"
+      }
+    ]
   },
   "userPatterns": [
     {
       "name": "banner",
       "namePretty": "Banner",
-      "path": "/Users/thomasjbradley/Dropbox/learn-the-web/web-dev-4/geohub/patterns/banner",
+      "path": "/Users/harryjeong/Documents/Semester 04/Web Development 04/week 10/geohub/patterns/banner",
       "html": [
         {
           "name": "banner",
           "namePretty": "Banner",
-          "filename": "banner",
-          "path": "/Users/thomasjbradley/Dropbox/learn-the-web/web-dev-4/geohub/patterns/banner/banner.html",
+          "path": "/Users/harryjeong/Documents/Semester 04/Web Development 04/week 10/geohub/patterns/banner/banner.html",
           "localPath": "patterns/banner/banner.html"
         }
       ],
@@ -640,8 +603,7 @@ const patternManifest_1521465881686 = {
         {
           "name": "readme",
           "namePretty": "Readme",
-          "filename": "README",
-          "path": "/Users/thomasjbradley/Dropbox/learn-the-web/web-dev-4/geohub/patterns/banner/README.md",
+          "path": "/Users/harryjeong/Documents/Semester 04/Web Development 04/week 10/geohub/patterns/banner/README.md",
           "localPath": "patterns/banner/README.md"
         }
       ],
@@ -649,8 +611,7 @@ const patternManifest_1521465881686 = {
         {
           "name": "banner",
           "namePretty": "Banner",
-          "filename": "banner",
-          "path": "/Users/thomasjbradley/Dropbox/learn-the-web/web-dev-4/geohub/patterns/banner/banner.css",
+          "path": "/Users/harryjeong/Documents/Semester 04/Web Development 04/week 10/geohub/patterns/banner/banner.css",
           "localPath": "patterns/banner/banner.css"
         }
       ]
@@ -658,13 +619,12 @@ const patternManifest_1521465881686 = {
     {
       "name": "buttons",
       "namePretty": "Buttons",
-      "path": "/Users/thomasjbradley/Dropbox/learn-the-web/web-dev-4/geohub/patterns/buttons",
+      "path": "/Users/harryjeong/Documents/Semester 04/Web Development 04/week 10/geohub/patterns/buttons",
       "html": [
         {
           "name": "buttons",
           "namePretty": "Buttons",
-          "filename": "buttons",
-          "path": "/Users/thomasjbradley/Dropbox/learn-the-web/web-dev-4/geohub/patterns/buttons/buttons.html",
+          "path": "/Users/harryjeong/Documents/Semester 04/Web Development 04/week 10/geohub/patterns/buttons/buttons.html",
           "localPath": "patterns/buttons/buttons.html"
         }
       ],
@@ -672,8 +632,7 @@ const patternManifest_1521465881686 = {
         {
           "name": "readme",
           "namePretty": "Readme",
-          "filename": "README",
-          "path": "/Users/thomasjbradley/Dropbox/learn-the-web/web-dev-4/geohub/patterns/buttons/README.md",
+          "path": "/Users/harryjeong/Documents/Semester 04/Web Development 04/week 10/geohub/patterns/buttons/README.md",
           "localPath": "patterns/buttons/README.md"
         }
       ],
@@ -681,8 +640,7 @@ const patternManifest_1521465881686 = {
         {
           "name": "buttons",
           "namePretty": "Buttons",
-          "filename": "buttons",
-          "path": "/Users/thomasjbradley/Dropbox/learn-the-web/web-dev-4/geohub/patterns/buttons/buttons.css",
+          "path": "/Users/harryjeong/Documents/Semester 04/Web Development 04/week 10/geohub/patterns/buttons/buttons.css",
           "localPath": "patterns/buttons/buttons.css"
         }
       ]
@@ -690,13 +648,12 @@ const patternManifest_1521465881686 = {
     {
       "name": "cards",
       "namePretty": "Cards",
-      "path": "/Users/thomasjbradley/Dropbox/learn-the-web/web-dev-4/geohub/patterns/cards",
+      "path": "/Users/harryjeong/Documents/Semester 04/Web Development 04/week 10/geohub/patterns/cards",
       "html": [
         {
           "name": "index",
           "namePretty": "Index",
-          "filename": "index",
-          "path": "/Users/thomasjbradley/Dropbox/learn-the-web/web-dev-4/geohub/patterns/cards/index.html",
+          "path": "/Users/harryjeong/Documents/Semester 04/Web Development 04/week 10/geohub/patterns/cards/index.html",
           "localPath": "patterns/cards/index.html",
           "readme": {
             "width": 400
@@ -707,8 +664,7 @@ const patternManifest_1521465881686 = {
         {
           "name": "readme",
           "namePretty": "Readme",
-          "filename": "README",
-          "path": "/Users/thomasjbradley/Dropbox/learn-the-web/web-dev-4/geohub/patterns/cards/README.md",
+          "path": "/Users/harryjeong/Documents/Semester 04/Web Development 04/week 10/geohub/patterns/cards/README.md",
           "localPath": "patterns/cards/README.md"
         }
       ],
@@ -716,8 +672,7 @@ const patternManifest_1521465881686 = {
         {
           "name": "cards",
           "namePretty": "Cards",
-          "filename": "cards",
-          "path": "/Users/thomasjbradley/Dropbox/learn-the-web/web-dev-4/geohub/patterns/cards/cards.css",
+          "path": "/Users/harryjeong/Documents/Semester 04/Web Development 04/week 10/geohub/patterns/cards/cards.css",
           "localPath": "patterns/cards/cards.css"
         }
       ]
@@ -725,13 +680,12 @@ const patternManifest_1521465881686 = {
     {
       "name": "footer",
       "namePretty": "Footer",
-      "path": "/Users/thomasjbradley/Dropbox/learn-the-web/web-dev-4/geohub/patterns/footer",
+      "path": "/Users/harryjeong/Documents/Semester 04/Web Development 04/week 10/geohub/patterns/footer",
       "html": [
         {
           "name": "footer",
           "namePretty": "Footer",
-          "filename": "footer",
-          "path": "/Users/thomasjbradley/Dropbox/learn-the-web/web-dev-4/geohub/patterns/footer/footer.html",
+          "path": "/Users/harryjeong/Documents/Semester 04/Web Development 04/week 10/geohub/patterns/footer/footer.html",
           "localPath": "patterns/footer/footer.html"
         }
       ],
@@ -739,8 +693,7 @@ const patternManifest_1521465881686 = {
         {
           "name": "readme",
           "namePretty": "Readme",
-          "filename": "README",
-          "path": "/Users/thomasjbradley/Dropbox/learn-the-web/web-dev-4/geohub/patterns/footer/README.md",
+          "path": "/Users/harryjeong/Documents/Semester 04/Web Development 04/week 10/geohub/patterns/footer/README.md",
           "localPath": "patterns/footer/README.md"
         }
       ],
@@ -748,8 +701,7 @@ const patternManifest_1521465881686 = {
         {
           "name": "footer",
           "namePretty": "Footer",
-          "filename": "footer",
-          "path": "/Users/thomasjbradley/Dropbox/learn-the-web/web-dev-4/geohub/patterns/footer/footer.css",
+          "path": "/Users/harryjeong/Documents/Semester 04/Web Development 04/week 10/geohub/patterns/footer/footer.css",
           "localPath": "patterns/footer/footer.css"
         }
       ]
@@ -757,13 +709,12 @@ const patternManifest_1521465881686 = {
     {
       "name": "header",
       "namePretty": "Header",
-      "path": "/Users/thomasjbradley/Dropbox/learn-the-web/web-dev-4/geohub/patterns/header",
+      "path": "/Users/harryjeong/Documents/Semester 04/Web Development 04/week 10/geohub/patterns/header",
       "html": [
         {
           "name": "header",
           "namePretty": "Header",
-          "filename": "header",
-          "path": "/Users/thomasjbradley/Dropbox/learn-the-web/web-dev-4/geohub/patterns/header/header.html",
+          "path": "/Users/harryjeong/Documents/Semester 04/Web Development 04/week 10/geohub/patterns/header/header.html",
           "localPath": "patterns/header/header.html"
         }
       ],
@@ -771,8 +722,7 @@ const patternManifest_1521465881686 = {
         {
           "name": "readme",
           "namePretty": "Readme",
-          "filename": "README",
-          "path": "/Users/thomasjbradley/Dropbox/learn-the-web/web-dev-4/geohub/patterns/header/README.md",
+          "path": "/Users/harryjeong/Documents/Semester 04/Web Development 04/week 10/geohub/patterns/header/README.md",
           "localPath": "patterns/header/README.md"
         }
       ],
@@ -780,8 +730,7 @@ const patternManifest_1521465881686 = {
         {
           "name": "header",
           "namePretty": "Header",
-          "filename": "header",
-          "path": "/Users/thomasjbradley/Dropbox/learn-the-web/web-dev-4/geohub/patterns/header/header.css",
+          "path": "/Users/harryjeong/Documents/Semester 04/Web Development 04/week 10/geohub/patterns/header/header.css",
           "localPath": "patterns/header/header.css"
         }
       ]
@@ -789,13 +738,12 @@ const patternManifest_1521465881686 = {
     {
       "name": "links",
       "namePretty": "Links",
-      "path": "/Users/thomasjbradley/Dropbox/learn-the-web/web-dev-4/geohub/patterns/links",
+      "path": "/Users/harryjeong/Documents/Semester 04/Web Development 04/week 10/geohub/patterns/links",
       "html": [
         {
           "name": "neutral",
           "namePretty": "Neutral",
-          "filename": "neutral",
-          "path": "/Users/thomasjbradley/Dropbox/learn-the-web/web-dev-4/geohub/patterns/links/neutral.html",
+          "path": "/Users/harryjeong/Documents/Semester 04/Web Development 04/week 10/geohub/patterns/links/neutral.html",
           "localPath": "patterns/links/neutral.html",
           "readme": {}
         }
@@ -804,8 +752,7 @@ const patternManifest_1521465881686 = {
         {
           "name": "readme",
           "namePretty": "Readme",
-          "filename": "README",
-          "path": "/Users/thomasjbradley/Dropbox/learn-the-web/web-dev-4/geohub/patterns/links/README.md",
+          "path": "/Users/harryjeong/Documents/Semester 04/Web Development 04/week 10/geohub/patterns/links/README.md",
           "localPath": "patterns/links/README.md"
         }
       ],
@@ -813,8 +760,7 @@ const patternManifest_1521465881686 = {
         {
           "name": "links",
           "namePretty": "Links",
-          "filename": "links",
-          "path": "/Users/thomasjbradley/Dropbox/learn-the-web/web-dev-4/geohub/patterns/links/links.css",
+          "path": "/Users/harryjeong/Documents/Semester 04/Web Development 04/week 10/geohub/patterns/links/links.css",
           "localPath": "patterns/links/links.css"
         }
       ]
@@ -840,5 +786,5 @@ const patternManifest_1521465881686 = {
   }
 };
 
-patternBotIncludes(patternManifest_1521465881686);
+patternBotIncludes(patternManifest_1521998665981);
 }());
